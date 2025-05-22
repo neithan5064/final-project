abebbc7e595ca40d2965d537ffc34c1e7400c42b
neithan pilay <neithan5064@gmail.com>
2025-05-22 17:55:49 -0400

    poted the dat in the main page using the getData method so now every data and history gets shown in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
 import AddTodo from "@/componets/todos/AddTodo";
+import { prisma } from "@/utils/prisma";
 
+async function getData(){
+  const data = await prisma.todo.findMany({
+    select: {
+    title: true,
+    id:true,
+    isCompleted: true,
+  },
+  orderBy: {
+    createAt: "asc",
+  },
+  });
 
-export default function Home() {
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-   <div className= "b-screen py-20 flex justify-center flex-col items0center">
+   <div className= "w-screen py-20 flex justify-center flex-col items0center">
     <span className="text-4xl font-extrabold uppercase">Todo App</span>
     <h1 className="text-5xl font-extrabold uppercase mb-5 text center">
       <span className="lowercase">w/</span>server actions
     </h1>
 
     <div className="flex justify-center flex-col items-center">
-      {/*addTodo  items*/}
-      <AddTodo/>
-      {/* map todos*/}
+      <AddTodo />
+      
+      <div className= "flex flex-col gap-5 items-center justify-center mt-10 2-screen">
+        {data.map((todo, id) => (
+          <div key={id}>{todo.title}</div>
+        ))}
+       </div>
     </div>
    </div>
   );
